618f7947771b096f35b4f998bce0f6aef2d563a6
Matyáš Strelec <matyasstrelec@gmail.com>
2023-10-27 18:24:36 +0200

    placeholder site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="flex w-full min-h-screen">
+	<div class="hero bg-base-200">
+		<div class="hero-content text-left">
+			<div class="w-full">
+				<h3 class="text-lg font-bold py-6">Hi there, I am</h3>
+				<h1 class="text-5xl font-bold">Matyáš Strelec</h1>
+				<p class="text-lg py-6">
+					I'm a computer science student at BUT FIT.<br />This website is still under construction.
+				</p>
+				<p class="text-lg">
+					You can find me on
+					<a class="link" href="https://github.com/mstrlc">GitHub</a>,
+					<br />
+					or contact me at
+					<a class="link" href="mailto:hello@mstrlc.eu">hello@mstrlc.eu</a>.
+				</p>
+			</div>
+		</div>
+	</div>
+</main>
